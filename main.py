36a21050a26a2fe8065962c2ac6d59274853bd07
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-22 01:27:53 +0700

    Deep Dive: Caching Small Integer Values

--- main.py
+++ main.py
@@ -170,3 +170,35 @@ if __name__ == "__main__":
   After the assignment m = n, m and n both point to the same object, confirmed by the fact that id(m) and id(n) return the same number.
   Once m is reassigned to 400, m and n point to different objects with different identities.
   """
+
+  # Deep Dive: Caching Small Integer Values
+
+  """
+  From what you knwow about variable assignment and object references in Python, the following probably won't surprise you:
+  """
+
+  m = 300
+  n = 300
+  print(id(m))
+  print(id(n))
+
+  """
+  With the statement m = 300, Python creates an integer object with the value 300 and sets m a reference to it.
+  n is then similarly assigned to an integer object with value 30 - but not the same object.
+  Thus, they have different identities, which you can verify from the values returned by id().
+
+  But consider this:
+  """
+
+  m = 300
+  n = 300
+  print(id(m))
+  print(id(n))
+
+  """
+  Here, m and n are separately assigned to integer objects having value 30.
+  But in this case, id(m) and id(n) are identical!
+
+  For purposes of optimization, the interpreter creates objects for the integers in the range [-5, 256] at startup, and then reuses them during program execution.
+  This, when you assign separate variables to an integer value in this range, they will actually reference the same object.
+  """
